7e7ba42ce4b8e7aac0bedb43528f4a29c99bd172
Tanbhir Ahmad <tanbhirahmadsiddiqui@gmail.com>
2026-03-25 15:37:39 +0500

    add apps to local storage

--- src/Components/CardDetails/CardDetails.jsx
+++ src/Components/CardDetails/CardDetails.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
+import { addToLocalStorage } from "../../utilities/addToLocalStorage";
 
 const CardDetails = () => {
   const { id } = useParams();
   const apps = useLoaderData();
   const cardDetails = apps.find((app) => app.id === parseInt(id));
-  console.log(cardDetails);
+  const { image, title } = cardDetails;
+  //   add to local Storage function
+  const handleAddToLocalStorage = (id) => {
+    // this function call from utilities
+    addToLocalStorage(id);
+  };
   return (
-    <div>
-      <h1>ami card Details</h1>
+    <div className="max-w-300 mx-auto">
+      <div className="flex">
+        <div className="w-87.5 h-87.5">
+          <img src={image} alt={title} />
+        </div>
+        <div>
+          <h1>{title}</h1>
+          <button
+            onClick={() => handleAddToLocalStorage(id)}
+            className="btn btn-primary"
+          >
+            Install
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/utilities/addToLocalStorage.js
@@ -0,0 +1,32 @@
+// this function is create for getting item in the local storage
+const getStoredApps = () => {
+  // get items from local Storage
+  const storedAppsString = localStorage.getItem("appsList");
+  //   if condition true then convert json.parse and return. first time condition must be false
+  if (storedAppsString) {
+    const storeAppsList = JSON.parse(storedAppsString);
+    return storeAppsList;
+  }
+  //   only first time else works and return an empty array
+  else {
+    return [];
+  }
+};
+
+// This function create to add the local storage
+const addToLocalStorage = (id) => {
+  // getting already stored local storage items
+  const storeAppsList = getStoredApps();
+  //   to check item includes in the local storage or not. if here then show alert
+  if (storeAppsList.includes(id)) {
+    alert("Apps Already Stored");
+  }
+  //   if not here to local storage then convert to json.stringify and set to local storage
+  else {
+    storeAppsList.push(id);
+    const app = JSON.stringify(storeAppsList);
+    localStorage.setItem("appsList", app);
+  }
+};
+// export functions for access any components
+export { addToLocalStorage };
