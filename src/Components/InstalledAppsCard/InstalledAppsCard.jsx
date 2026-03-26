6ce322d9649e4e6b1a5df9a7bbf90b83e3bac415
Tanbhir Ahmad <tanbhirahmadsiddiqui@gmail.com>
2026-03-26 09:58:26 +0600

    apply sort button to the installation page

--- src/Components/InstalledAppsCard/InstalledAppsCard.jsx
+++ src/Components/InstalledAppsCard/InstalledAppsCard.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 const InstalledAppsCard = ({ installApp }) => {
+  const { image, title, downloads, ratingAvg, size } = installApp;
   return (
-    <div>
-      <h1>InstalledAppsCard</h1>
+    <div className="border my-3 flex justify-between items-center">
+      <div className="flex gap-5 p-5 items-center">
+        <div>
+          <img className="w-20 h-20" src={image} alt={title} />
+        </div>
+        <div className="flex flex-col gap-3">
+          <div>{title}</div>
+          <div className="flex gap-5">
+            <span>{downloads}</span>
+            <span>{ratingAvg}</span>
+            <span>{size}</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button className="btn btn-primary me-5">Uninstall</button>
+      </div>
     </div>
   );
 };
--- src/Pages/Installation/Installation.jsx
+++ src/Pages/Installation/Installation.jsx
@@ -4,26 +4,64 @@ import { getStoredApps } from "../../utilities/addToLocalStorage";
 import InstalledAppsCard from "../../Components/InstalledAppsCard/InstalledAppsCard";
 
 const Installation = () => {
+  const [sortButton, setSortButton] = useState("Sort By Size ⬇️");
+  const [sort, setSort] = useState("");
   const [installedApps, setInstallApps] = useState([]);
   const apps = useLoaderData();
   useEffect(() => {
     const getStoredAppsList = getStoredApps();
     const convertStoredIdTOInt = getStoredAppsList.map((id) => parseInt(id));
-    console.log(convertStoredIdTOInt);
+
     const installAppsList = apps.filter((app) =>
       convertStoredIdTOInt.includes(app.id),
     );
     setInstallApps(installAppsList);
   }, []);
+
+  const handleSort = (type) => {
+    setSort(type);
+    if (type === "LowToHigh") {
+      const highToLow = [...installedApps].sort((a, b) => a.size - b.size);
+      setInstallApps(highToLow);
+      setSortButton("Low To High");
+    }
+    if (type === "HighToLow") {
+      const highToLow = [...installedApps].sort((a, b) => b.size - a.size);
+      setInstallApps(highToLow);
+      setSortButton("High To Low");
+    }
+  };
   return (
     <div>
-      <h1>Install Apps: {installedApps.length}</h1>
-      {installedApps.map((installApp) => (
-        <InstalledAppsCard
-          key={installApp.id}
-          installApp={installApp}
-        ></InstalledAppsCard>
-      ))}
+      <div className="flex justify-between">
+        <div>
+          <h1>Install Apps: {installedApps.length}</h1>
+        </div>
+        <div className="dropdown dropdown-start">
+          <div tabIndex={0} role="button" className="btn m-1">
+            {sortButton}
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li>
+              <a onClick={() => handleSort("LowToHigh")}>Low To High</a>
+            </li>
+            <li>
+              <a onClick={() => handleSort("HighToLow")}>High To Low</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        {installedApps.map((installApp) => (
+          <InstalledAppsCard
+            key={installApp.id}
+            installApp={installApp}
+          ></InstalledAppsCard>
+        ))}
+      </div>
     </div>
   );
 };
